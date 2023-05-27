5664f5f8ce64c048dfd9168d648581f3236ad9b8
Kiara Granados <98955223+k14ra@users.noreply.github.com>
2023-05-27 18:04:10 +0200

    Update CityRepository.java

--- repository/CityRepository.java
+++ repository/CityRepository.java
@@ -11,6 +11,8 @@ public class CityRepository {
 	
 	private static CityRepository instance;
 	
+	//synchronized es un modificador que asegura atomicidad, es decir
+	//solo puede ser accedido por un hilo de ejecucion a la vez.
 	public static synchronized CityRepository getInstance() {
 		if(instance == null) {
 			instance = new CityRepository();
